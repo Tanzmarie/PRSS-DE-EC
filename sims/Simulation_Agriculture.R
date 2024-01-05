2ee0fa7a0f4b13159a6a70ce0dfa178b187e2b39
Tanzmarie <you@example.com>
2024-01-05 19:27:51 +0100

    adjust sims 2

--- sims/Simulation_Agriculture.R
+++ sims/Simulation_Agriculture.R
@@ -1,5 +1,6 @@
 # Dependencies
 library(rstan)
+library(knitr)
 
 options(mc.cores = parallel::detectCores())
 
@@ -12,7 +13,7 @@ source("functions/Costs.R")
 load("prevalence.RData")
 
 # Find the prevalence value in region u_i in time t_i
-target_time_point <- 1
+target_time_point <- 500
 location_point <- c(0.1,0.8)
 
 closest_location <- find_closest_location(simulated_data, location_point)
@@ -35,7 +36,7 @@ print(result_tests)
 # Using MCMC to capture Uncertainty of Economic Costs
 # Simulate data from LN(40, 4)
 set.seed(100)  # Set seed for reproducibility
-income_data <- rlnorm(1000, meanlog = log(15), sdlog = sqrt(3))
+income_data <- rlnorm(500, meanlog = log(15), sdlog = sqrt(3))
 
 # Define the Stan model
 stan_code <- '
@@ -92,10 +93,10 @@ res = exp(res)
 
 # Costs
 cv = 1000
-cm = 50
-cp = 100
-cl = 50
-tau0 = 800
+cm = 25
+cp = 50
+cl = 25
+tau0 = 850
 
 tau = unlist(result_tests["Tests"])
 omega = unlist(result_tests["Waiting.Times"])
@@ -103,9 +104,10 @@ omega = unlist(result_tests["Waiting.Times"])
 h = 1
 n = 1000
 mu = res[1]
-k = n * prevalence_at_target
-co = 200
+k = round(n * prevalence_at_target)
+co = 150
 
 result_costs <- calculateEconomicCosts(cv, cm, cp, cl, tau, tau0, h, omega, n, mu, k, co)
 print(result_costs)
 
+kable(result_costs, format = "latex")
